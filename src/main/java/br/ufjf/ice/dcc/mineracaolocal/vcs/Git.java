641f8054dd1cacddbf5891677fa4f2b6c4f0a91e
Gleiph <gleiphgh@gmail.com>
2021-02-11 22:04:31 -0200

    Example

--- src/main/java/br/ufjf/ice/dcc/mineracaolocal/vcs/Git.java
+++ src/main/java/br/ufjf/ice/dcc/mineracaolocal/vcs/Git.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  * @author gleiph
  */
 public class Git {
- 
+
     public static List<String> listVersionsSHA(String repositoryPath) {
 
         try {
@@ -38,26 +38,27 @@ public class Git {
         return null;
     }
 
-    public static  List<String> listDevelopers(String repositoryPath){
+    public static List<String> listDevelopers(String repositoryPath) {
         List<String> developers = new ArrayList<>();
-        
+
         for (String commiter : listCommiters(repositoryPath)) {
-            if(!developers.contains(commiter))
+            if (!developers.contains(commiter)) {
                 developers.add(commiter);
-            
+            }
+
         }
-        
+
         for (String author : listAuthors(repositoryPath)) {
-            if(!developers.contains(author))
+            if (!developers.contains(author)) {
                 developers.add(author);
+            }
         }
-        
-        
+
         return developers;
     }
-    
-    public static List<String> listCommiters(String repositoryPath){
-        
+
+    public static List<String> listCommiters(String repositoryPath) {
+
         List<String> commiters = null;
         try {
             CLIExecution execution = CLIExecute.execute("git log --pretty=%cn", repositoryPath);
@@ -67,8 +68,9 @@ public class Git {
             } else {
                 commiters = new ArrayList<>();
                 for (String commiter : execution.getOutput()) {
-                    if(!commiters.contains(commiter))
+                    if (!commiters.contains(commiter)) {
                         commiters.add(commiter);
+                    }
                 }
                 return commiters;
             }
@@ -78,9 +80,9 @@ public class Git {
         }
         return null;
     }
-    
-    public static List<String> listAuthors(String repositoryPath){
-        
+
+    public static List<String> listAuthors(String repositoryPath) {
+
         List<String> authors = null;
         try {
             CLIExecution execution = CLIExecute.execute("git log --pretty=%an", repositoryPath);
@@ -90,8 +92,9 @@ public class Git {
             } else {
                 authors = new ArrayList<>();
                 for (String commiter : execution.getOutput()) {
-                    if(!authors.contains(commiter))
+                    if (!authors.contains(commiter)) {
                         authors.add(commiter);
+                    }
                 }
                 return authors;
             }
@@ -101,7 +104,7 @@ public class Git {
         }
         return null;
     }
-    
+
     public static List<String> checkout(String repositoryPath, String sha) {
 
         try {
@@ -119,7 +122,7 @@ public class Git {
 
         return null;
     }
-    
+
     public static List<String> reset(String repositoryPath) {
 
         try {
@@ -137,7 +140,7 @@ public class Git {
 
         return null;
     }
-    
+
     public static List<String> clean(String repositoryPath) {
 
         try {
@@ -155,5 +158,45 @@ public class Git {
 
         return null;
     }
-    
+
+    public static List<String> diff(String repositoryPath, String sha1, String sha2) {
+
+        try {
+            CLIExecution execution = CLIExecute.execute("git diff " + sha1 + " " + sha2, repositoryPath);
+
+            if (execution.getError() != null && !execution.getError().isEmpty()) {
+                return null;
+            } else {
+                return execution.getOutput();
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Mineracao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String path = "/Users/gleiph/repositories/cobaia";
+        String sha1 = "302f11028106bdc0535da127d2b49cda61f9eda5";
+        String sha2 = "e860773eb86f1ffdb0ddbcc311458eadcb0c167f";
+
+        int added = 0, removed = 0;
+
+        List<String> lines = diff(path, sha1, sha2);
+
+        for (String line : lines) {
+            
+            if(line.startsWith("+") && !line.startsWith("++"))
+                added++;
+            else if(line.startsWith("-") && !line.startsWith("--"))
+                removed++;
+            System.out.println(line);
+        }
+
+        System.out.println("removed = " + removed);
+        System.out.println("added = " + added);
+    }
+
 }
